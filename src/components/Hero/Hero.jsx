5c4978dc5d67573331ec6f55fcc7144c6be4debf
IndunilRanganath <indunilranganath071@gmail.com>
2023-10-18 14:24:17 +0530

    Programs card created

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './Hero.css'
 import Header from '../Header/Header'
+import hero_image from '../../assets/hero_image.png'
+import hero_image_back from '../../assets/hero_image_back.png'
+import Heart from '../../assets/heart.png'
+import Calories from '../../assets/calories.png'
 
 export const Hero = () => {
   return (
@@ -21,12 +25,59 @@ export const Hero = () => {
           <div>
             <span>ideal  body</span>
           </div>
-          <div></div>
+          <div>
+            <span>In the we will help you to shape and build your udeal body and live up your life to fullest</span>
+          </div>
+        </div>
+
+        {/* figures */}
+        <div className="figures">
+            <div>
+                <span>+140</span>
+                <span>expert coachs</span>
+            </div>
+            <div>
+                <span>+978</span>
+                <span>members joined</span>
+            </div>
+            <div>
+                <span>+50</span>
+                <span>fitness programs</span>
+            </div>
+        </div>
+
+        {/* Hero button */}
+        <div className='hero-button'>
+          <button className='btn'>Get Start</button>
+          <button className='btn'>Learn more</button>
         </div>
 
       </div>
 
-      <div className="right-h">Right</div>
+      <div className="right-h">
+        <button className='btn'>Join Now</button>
+
+        <div className='heart-rate'>
+          <img src={Heart} alt="heart" />
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </div> 
+
+        {/* hero image */}
+        <img src={hero_image} alt="hero" className='hero-image' />
+        <img src={hero_image_back} alt="hero" className='hero-image-back' />
+
+        {/* calories */}
+
+        <div className='calories'>
+          <img src={Calories} alt="calories" />
+          <div>
+            <span>Calories Burned</span>
+            <span>516 kcal</span>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
