8754f7ad1a88aababaeea1792fca737506a6847d
IndunilRanganath <indunilranganath071@gmail.com>
2023-11-03 17:12:57 +0530

    added Plans and Reasons components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import React from 'react';
 import { Hero } from './components/Hero/Hero';
 import Programs from './components/Programs/Programs';
+import Reasons from './components/Reasons/Reasons';
+import Plans from './components/Plans/Plans';
 
 function App() {
   return (
     <div className="App">
       <Hero />
       <Programs />
+      <Reasons />
+      <Plans />
     </div>
   );
 }
